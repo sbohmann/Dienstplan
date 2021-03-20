4ee58797578a08b3a167e404b7b410e1ecc76acf
sbohmann <mail@sebastian-bohmann.com>
2021-03-20 20:41:49 +0100

    Adding launch script, extended .gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
-let weekday_name = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+let weekdayName = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+
+let storedMonthFileContent = fs.readFileSync('test_data/2021/2021-08.json', 'utf8')
+
+let storedMonth = JSON.parse(storedMonthFileContent)
 
 let rows = []
-
-for (let day = 1, weekday = 0; day <= 31; ++day, weekday = (weekday + 1) % 7) {
-    if (day > 0 && weekday === 1) {
+for (let dayIndex = 0; dayIndex < storedMonth.days.length; ++dayIndex) {
+    let day = storedMonth.days[dayIndex]
+    // TODO get the date from storedMonth.year, storedMonth.month, and day.day
+    if (dayIndex > 0 && day.dayOfWeek === 'Mo') { // TODO replace check for 'Mo' with week day lookup for the date
         rows.push({week_change: true})
     }
     let row = {
-        day: day,
-        weekday: weekday_name[weekday],
-        name: "NAME_" + day,
-        reserve: "RESERVE_" + day,
-        second_reserve: "SECOND_RESERVE_" + day,
-        sunday: weekday === 0
+        day: day.dayOfMonth,
+        weekday: day.dayOfWeek,
+        name: day.primary ? day.primary.name : "",
+        reserve: day.reserve ? day.reserve.name : "",
+        second_reserve: day.secondReserve ? day.secondReserve.name : "",
+        sunday: day.dayOfWeek === 'So' // TODO replace check for 'Mo' with week day lookup for the date
     }
     rows.push(row)
 }
