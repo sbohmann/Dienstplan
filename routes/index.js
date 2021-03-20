730363a16069d23ca4d380c1ee5b44dd6d301b94
sbohmann <mail@sebastian-bohmann.com>
2021-03-20 18:38:07 +0100

    Adding launch script, extended .gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const router = express.Router()
 
+let weekday_name = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+
+let rows = []
+
+for (let day = 1, weekday = 0; day <= 31; ++day, weekday = (weekday + 1) % 7) {
+    if (day > 0 && weekday === 1) {
+        rows.push({week_change: true})
+    }
+    let row = {
+        day: day,
+        weekday: weekday_name[weekday],
+        name: "NAME_" + day,
+        reserve: "RESERVE_" + day,
+        second_reserve: "SECOND_RESERVE_" + day,
+        sunday: weekday === 0
+    }
+    rows.push(row)
+}
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: "August 2021",
         month: "August",
         rows: [
-            {weekday: "So", day: 1, sunday: true, reserve: "RESERVE_1"},
-            {week_change: true},
-            {weekday: "Mo", day: 2, name: "NAME_2", reserve: "RESERVE_2"},
-            {weekday: "Di", day: 3, name: "NAME_2"},
-            {weekday: "Mi", day: 4, reserve: "RESERVE_4"}
-        ]
+            ...rows]
     })
 })
 
