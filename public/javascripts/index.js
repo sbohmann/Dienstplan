1e8b7451f0733f785744c97b9c20017f66f99d7b
sbohmann <mail@sebastian-bohmann.com>
2021-04-24 13:05:55 +0200

    Fixed a comment

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -25,27 +25,3 @@ function addSecondaryReserve(day, id, name) {
 function removeSecondaryReserve(day, id, name) {
     alert("remove secondary reserve user " + id + " (" + name + ") for day " + day)
 }
-
-function buildTableContent() {
-    let days = JSON.parse(initialData)
-    let table = document.getElementById('monats-dienstplan')
-
-    for (let day of days) {
-        let row = document.createElement('tr')
-        row.appendChild(dayOWeek(day))
-        row.appendChild(dayOMonth(day))
-        table.appendChild(row)
-    }
-}
-
-function dayOWeek(day) {
-    let cell = document.createElement('td')
-    cell.appendChild(document.createTextNode(day.dayOfWeek))
-    return cell
-}
-
-function dayOMonth(day) {
-    let cell = document.createElement('td')
-    cell.appendChild(document.createTextNode(day.dayOfMonth + ". "))
-    return cell
-}
