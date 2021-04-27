c73a1b7da7233343a1dc0c423a4c140a3966a451
sbohmann <mail@sebastian-bohmann.com>
2021-04-27 14:46:04 +0200

    Adding / removing

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,53 +2,49 @@ window.onload = () => {
     fillTableContent()
 }
 
-function addPrimary(day, id, name) {
-    alert("add primary user " + id + " (" + name + ") for day " + day)
+let year
+let month
+let userId
+
+function add(day, id, context) {
+    alert("add " + context + " user " + id + " for day " + day)
 }
 
-function removePrimary(day, id, name) {
-    alert("remove primary user " + id + " (" + name + ") for day " + day)
-}
-
-function addReserve(day, id, name) {
-    alert("add reserve user " + id + " (" + name + ") for day " + day)
-}
-
-function removeReserve(day, id, name) {
-    alert("remove reserve user " + id + " (" + name + ") for day " + day)
-}
-
-function addSecondaryReserve(day, id, name) {
-    alert("add secondary reserve user " + id + " (" + name + ") for day " + day)
-}
-
-function removeSecondaryReserve(day, id, name) {
-    alert("remove secondary reserve user " + id + " (" + name + ") for day " + day)
+function remove(day, id, context) {
+    alert("remove " + context + " user " + id + " for day " + day)
 }
 
 function fillTableContent() {
     const request = new XMLHttpRequest()
     request.open('GET', '/data', true)
-    request.onload =() => {
-        const initialData =request.responseText
-        const days = JSON.parse(initialData)
+    request.onload = () => {
+        const data = JSON.parse(request.responseText)
+        year = data.year
+        month = data.month
+        userId = data.userId
+        const days = data.days
         for (let dayIndex = 0; dayIndex < days.length; ++dayIndex) {
             const dayOfMonth = dayIndex + 1
             const day = days[dayIndex]
-            fill('primary_' + dayOfMonth, day.primary, dayOfMonth)
-            fill('reserve_' + dayOfMonth, day.reserve, dayOfMonth)
-            fill('secondReserve_' + dayOfMonth, day.secondReserve, dayOfMonth)
+            fill(day.primary, dayOfMonth, "primary")
+            fill(day.reserve, dayOfMonth, "reserve")
+            fill(day.secondReserve, dayOfMonth, "secondReserve")
         }
     }
     request.send()
 }
 
-function fill(id, data, dayOfMonth) {
+function fill(data, dayOfMonth, context) {
+    const id = context + '_' + dayOfMonth
+    let cell = document.getElementById(id)
     if (data) {
-        let cell = document.getElementById(id)
         cell.textContent = data.name
         cell.classList.remove("add-button")
         cell.classList.add("remove-button")
-        cell.onclick = () => removePrimary(dayOfMonth, data.id, data.name)
+        cell.onclick = () => remove(dayOfMonth, data.id, context)
+    } else {
+        cell.classList.remove("remove-button")
+        cell.classList.add("add-button")
+        cell.onclick = () => add(dayOfMonth, userId, context)
     }
 }
--- routes/data.js
+++ routes/data.js
@@ -6,8 +6,24 @@ const year = 2021
 const month = 8
 
 router.get('/', function (req, res, next) {
-    const monthData = storage.data.years[year][month]
+    let days = storage.data.years[year][month]
+    const monthData = {
+        year,
+        month,
+        userId: 80776, // TODO replace with user ID from session context
+        days
+    }
     res.json(monthData)
+})
+
+router.post('/add', function (req, res, next) {
+    console.log("add: " + req.body)
+    res.json("add response")
+})
+
+router.post('/remove', function (req, res, next) {
+    console.log("remove: " + req.body)
+    res.json("remove response")
 })
 
 module.exports = router
