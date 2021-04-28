88ed217f402f398efc7a1b8ef05fb6a9a264546d
sbohmann <mail@sebastian-bohmann.com>
2021-04-28 15:08:46 +0200

    Login with user ID

--- app.js
+++ app.js
@@ -14,27 +14,27 @@ const storage = require('./storage/storage')
 const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
   secret: storage.data.sessionSecret,
   name: 'sessionId'
 }))
 
-app.use('/', indexRouter);
-app.use('/data', dataRouter);
-app.use('/login', loginRouter);
+app.use('/', indexRouter)
+app.use('/data', dataRouter)
+app.use('/login', loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -42,12 +42,12 @@ app.use(function(err, req, res, next) {
   console.error(err.stack)
 
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const storage = require('../storage/storage')
 
 router.get('/', function (req, res, next) {
     res.render('login')
 })
 
 router.post('/', function (req, res, next) {
-    req.session.userId = 80776
-    res.status(302)
-    res.set('Location', '/')
-    res.send()
+    let userId = parseInt(req.body.user)
+    if (storage.userForId.has(userId)) {
+        req.session.userId = userId
+        res.status(302)
+        res.set('Location', '/')
+        res.send()
+    } else {
+        res.status(302)
+        res.set('Location', '/login')
+        res.send()
+    }
 })
 
 module.exports = router
