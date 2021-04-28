20b60daa50d654ad0ead3061409b4bd9aa90ed2a
sbohmann <mail@sebastian-bohmann.com>
2021-04-28 16:10:51 +0200

    Login with password

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const storage = require('../storage/storage')
+const bcrypt = require('bcrypt')
 
 router.get('/', function (req, res, next) {
     res.render('login')
@@ -8,7 +9,7 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     let userId = parseInt(req.body.user)
-    if (storage.userForId.has(userId)) {
+    if (checkLogin(userId, req.body.password)) {
         req.session.userId = userId
         res.status(302)
         res.set('Location', '/')
@@ -19,5 +20,17 @@ router.post('/', function (req, res, next) {
         res.send()
     }
 })
+
+function checkLogin(userId, password) {
+    let user = storage.userForId.get(userId)
+    if (!user) {
+        return false
+    }
+    if (!user.salt || !user.hash) {
+        return false
+    }
+    let calculatedHash = bcrypt.hashSync(password, user.salt)
+    return calculatedHash === user.hash
+}
 
 module.exports = router
