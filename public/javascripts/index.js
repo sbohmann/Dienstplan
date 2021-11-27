82d788e1429d207753c29850de9bae316c11b26c
sbohmann <mail@sebastian-bohmann.com>
2021-11-27 19:50:16 +0100

    Removed a redundant line of code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -72,6 +72,7 @@ function fillTableContent(postAction) {
             fill(day.secondReserve, dayOfMonth, "secondReserve")
         }
         if (postAction) {
+            console.log(postAction)
             postAction()
         }
     }
