8a82760bcc73a1ed304e60df34ada2ef0200182d
sbohmann <mail@sebastian-bohmann.com>
2021-11-28 22:18:54 +0100

    Menu

--- routes/menu.js
+++ routes/menu.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const storage = require('../storage/storage.js')
 
 router.get('/menu', function (req, res, next) {
     let userId = req.session.userId
