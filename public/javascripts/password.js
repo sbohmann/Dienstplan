01dc18c6f1fa08baa28ae03ad89c5a8c3251c437
sbohmann <mail@sebastian-bohmann.com>
2021-12-05 13:09:30 +0100

    password change

---
+++ public/javascripts/password.js
@@ -0,0 +1,11 @@
+let currentPasswordInput
+let newPasswordInput
+let repeatPasswordInput
+let changePasswordButton
+
+window.onload = () => {
+    currentPasswordInput = document.getElementById('current')
+    newPasswordInput = document.getElementById('new')
+    repeatPasswordInput = document.getElementById('repetition')
+    changePasswordButton = document.getElementById('changePassword')
+}
--- routes/password.js
+++ routes/password.js
@@ -17,39 +17,56 @@ router.get('/', function (req, res, next) {
 })
 
 router.post('/', function (req, res, next) {
-    if (changePassword(userName, req)) {
-        res.status(302)
-        res.set('Location', '/')
-        res.send()
-    } else {
-        console.log("Login failed for user [" + req.body.user + "]")
-        res.status(302)
-        res.set('Location', '/login')
-        res.send()
+    function changePassword() {
+        try {
+            if (changePassword(userName, req)) {
+                res.status(302)
+                res.set('Location', '/')
+                res.send()
+            } else {
+                console.log("Login failed for user [" + req.body.user + "]")
+                res.status(302)
+                res.set('Location', '/login')
+                res.send()
+            }
+        } catch (error) {
+
+        }
     }
+
+    function checkUserId() {
+        let userId = req.session.userId
+        if (!userId) {
+            res.status(302)
+            res.set('Location', '/login')
+            res.send()
+        }
+    }
+
+    changePassword()
 })
-//
-// function checkLogin(userName, req) {
-//     let userId = storage.userIdForUserName.get(userName)
-//     if (userId === undefined) {
-//         console.log("Unknown user name [" + userName + "]")
-//         return false
-//     }
-//     let user = storage.userForId.get(userId)
-//     if (user === undefined) {
-//         console.log("Unknown user ID [" + userId + "]")
-//         return false
-//     }
-//     if (user.salt === undefined || user.hash === undefined) {
-//         console.log("No password configured for user [" + userId + "]")
-//         return false
-//     }
-//     let calculatedHash = bcrypt.hashSync(req.body.password, user.salt)
-//     if (calculatedHash === user.hash) {
-//         req.session.userId = userId
-//         return true
-//     }
-//     return false
-// }
+
+function changePassword(userName, req) {
+    let userId = storage.userIdForUserName.get(userName)
+    if (userId === undefined) {
+        console.log("Unknown user name [" + userName + "]")
+        return false
+    }
+    let user = storage.userForId.get(userId)
+    if (user === undefined) {
+        console.log("Unknown user ID [" + userId + "]")
+        return false
+    }
+    if (user.salt === undefined || user.hash === undefined) {
+        console.log("No password configured for user [" + userId + "]")
+        return false
+    }
+    let calculatedHash = bcrypt.hashSync(req.body.password, user.salt)
+    if (calculatedHash === user.hash) {
+        req.session.userId = userId
+        return true
+    }
+    return false
+}
 
 module.exports = router
