462dab5091821b66a7aa6da30be853b7f4d7b80a
sbohmann <mail@sebastian-bohmann.com>
2021-12-08 17:06:55 +0100

    Preventing page scrolling in dialogs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -34,6 +34,12 @@ window.onload = () => {
             addUserButton.textContent = user.name + " (" + user.id + ")"
             selectionButtons.appendChild(addUserButton)
         }
+        for (let user of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+            const addUserButton = document.createElement('button')
+            addUserButton.classList.add('dialogButton')
+            addUserButton.textContent = user
+            selectionButtons.appendChild(addUserButton)
+        }
     })
 }
 
