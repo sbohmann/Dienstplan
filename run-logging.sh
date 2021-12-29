bc513491169ecf3d7d865460184410a31921c7ce
sbohmann <mail@sebastian-bohmann.com>
2021-12-29 19:05:33 +0100

    Added run_logging.sh

---
+++ run-logging.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+output_file="$1"
+shift
+echo Arguments for annotate-output: "[$@]"
+# echo in a subshell is required for opening the output file for each line
+annotate-output +%Y-%m-%dT%H:%M:%S.%N "$@" | while read line; do (echo "$line" >> "$output_file"); done
