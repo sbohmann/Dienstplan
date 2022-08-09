74448ef96e1ce8336b50e23200bc812b6f7645ff
sbohmann <mail@sebastian-bohmann.com>
2022-08-09 13:06:13 +0200

    Considering time of day for editing window

--- storage/relevantMonth.js
+++ storage/relevantMonth.js
@@ -1,19 +1,33 @@
 const joda = require('@js-joda/core')
 
-// TODO configuration
+// TODO configuration, plus add constant for the end of the editing period
 const RelevantMonthOffset = 5
 
 module.exports = () => {
-    const today = joda.LocalDate.now()
-    const offset = today.dayOfMonth() >= 15
+    const now = joda.LocalDateTime.now()
+    const offset = now.dayOfMonth() >= 15
         ? RelevantMonthOffset
         : RelevantMonthOffset - 1
-    const result = today
+    const result = now
         .withDayOfMonth(1)
         .plusMonths(offset)
     return {
         year: result.year(),
         month: result.month().value(),
-        editable: today.dayOfMonth() < 10 || today.dayOfMonth() >= 15
+        editable: insideEditingWindow(now)
+    }
+}
+
+function insideEditingWindow(now) {
+    if (now.dayOfMonth() > 15) {
+        return true
+    } else if (now.dayOfMonth() === 15) {
+        return now.hour() >= 21
+    } else if (now.dayOfMonth() < 10) {
+        return true
+    } else if (now.dayOfMonth() === 10) {
+        return now.hour() < 21
+    } else {
+        return false
     }
 }
